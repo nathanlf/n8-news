fab8bf6bdb233851f4a4860b6d16e0a5220e66fe
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-19 11:50:23 -0400

    remove unused heading css module

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Navbar from "./Navbar";
-import { container, heading, footer } from "./layout.module.css";
+import { container, footer } from "./layout.module.css";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
