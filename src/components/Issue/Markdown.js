ecf52d52542f4c27e10b0f1a7843edd8d7309cb5
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-20 12:15:44 -0400

    shrink container size, map markdown body elements

--- src/components/Issue/Markdown.js
+++ src/components/Issue/Markdown.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import SectionHeader from "./SectionHeader";
+import { Typography } from "@mui/joy";
 
 export const Markdown = ({ src }) => {
   return (
@@ -8,6 +9,15 @@ export const Markdown = ({ src }) => {
       children={src}
       components={{
         h1: ({ children }) => <SectionHeader title={children} />,
+        p: ({ children }) => (
+          <Typography level="body-md">{children}</Typography>
+        ),
+        strong: ({ children }) => (
+          <Typography level="title-lg">{children}</Typography>
+        ),
+        // li: ({ children }) => (
+        //   <Typography level="body-lg">{children}</Typography>
+        // ),
       }}
     />
   );
