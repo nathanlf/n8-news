f18d40a14aafb39046b0123e0902778e225f99d5
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-20 15:59:17 -0400

    redesign navbar wip

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,28 +1,43 @@
 import React from "react";
 import { Link } from "gatsby";
-import { navBar, navLinks, navLinkText } from "./layout.module.css";
+import { Typography } from "@mui/joy";
+import Tabs from "@mui/joy/Tabs";
+import Tab from "@mui/joy/Tab";
+import TabList from "@mui/joy/TabList";
 
 const Navbar = () => {
   return (
-    <nav className={navBar}>
-      <div className={navLinks}>
-        <Link to="/" className={navLinkText}>
-          Home
-        </Link>
-        <Link to="/about" className={navLinkText}>
-          About
-        </Link>
-        <Link to="/archive" className={navLinkText}>
-          Archive
-        </Link>
-        <Link to="/archive/2024-05/4.5" className={navLinkText}>
-          Newest Edition
-        </Link>
-        <Link to="/settings" className={navLinkText}>
-          Settings
-        </Link>
-      </div>
-    </nav>
+    <Tabs
+      defaultValue={0}
+      tabFlex="auto"
+      flex="initial"
+      component="nav"
+      sx={{
+        my: 2,
+      }}
+    >
+      <TabList>
+        <Tab>
+          <Link to="/">
+            <Typography level="h4" variant="soft" align="left">
+              Home
+            </Typography>
+          </Link>
+        </Tab>
+        <Tab>
+          <Link to="/archive/2024-05/4.5">Newest Edition</Link>
+        </Tab>
+        <Tab>
+          <Link to="/archive">Archive</Link>
+        </Tab>
+        <Tab>
+          <Link to="/about">About</Link>
+        </Tab>
+        <Tab>
+          <Link to="/settings">Settings</Link>
+        </Tab>
+      </TabList>
+    </Tabs>
   );
 };
 
