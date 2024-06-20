3ec87fc3449c0b712259af72dc3aaa4dd59199e2
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-20 15:15:11 -0400

    implement header component, replace static html image with gatsby static image component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Sheet } from "@mui/joy";
+import { StaticImage } from "gatsby-plugin-image";
+import { Box, Typography } from "@mui/joy";
 
 export const Header = ({ title }) => {
   return (
-    <Sheet>
-      <img
-        src="/renci-logo.png"
-        alt="RENCI Logo"
-        style={{ maxWidth: "100%" }}
-      />
-      {title}
-    </Sheet>
+    <Box
+      justifyContent="space-between"
+      component="header"
+      display="flex"
+      sx={{
+        py: 2,
+      }}
+    >
+      <StaticImage src="../../images/renci-logo.png" alt="RENCI Logo" />
+      <Typography
+        level="h1"
+        sx={{
+          mt: 4,
+        }}
+      >
+        {title}
+      </Typography>
+    </Box>
   );
 };
