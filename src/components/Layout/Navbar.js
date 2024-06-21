a5b9e993a56838dcd54c4dfa50b869316a7e82ca
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-21 10:52:57 -0400

    clean up navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,43 +1,43 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Typography } from "@mui/joy";
-import Tabs from "@mui/joy/Tabs";
-import Tab from "@mui/joy/Tab";
-import TabList from "@mui/joy/TabList";
+import Stack from "@mui/joy/Stack";
+import Divider from "@mui/material/Divider";
+import { navLinks } from "./layout.module.css";
 
 const Navbar = () => {
   return (
-    <Tabs
-      defaultValue={0}
-      tabFlex="auto"
-      flex="initial"
-      component="nav"
+    <Stack
+      role="navigation"
+      direction="row"
+      divider={<Divider orientation="vertical" flexItem size="lg" />}
+      alignItems="center"
+      justifyContent="center"
+      spacing={4}
       sx={{
         my: 2,
+        border: "1px",
+        borderRadius: "sm",
+        boxShadow: "md",
+        backgroundColor: "#fafafa",
+        py: 2,
       }}
     >
-      <TabList>
-        <Tab>
-          <Link to="/">
-            <Typography level="h4" variant="soft" align="left">
-              Home
-            </Typography>
-          </Link>
-        </Tab>
-        <Tab>
-          <Link to="/archive/2024-05/4.5">Newest Edition</Link>
-        </Tab>
-        <Tab>
-          <Link to="/archive">Archive</Link>
-        </Tab>
-        <Tab>
-          <Link to="/about">About</Link>
-        </Tab>
-        <Tab>
-          <Link to="/settings">Settings</Link>
-        </Tab>
-      </TabList>
-    </Tabs>
+      <Link to="/" className={navLinks}>
+        Home
+      </Link>
+      <Link to="/archive/2024-05/4.5" className={navLinks}>
+        Newest Edition
+      </Link>
+      <Link to="/archive" className={navLinks}>
+        Archive
+      </Link>
+      <Link to="/about" className={navLinks}>
+        About
+      </Link>
+      <Link to="/settings" className={navLinks}>
+        Settings
+      </Link>
+    </Stack>
   );
 };
 
