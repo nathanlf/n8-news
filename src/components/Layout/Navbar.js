fd6d4ad5b562f417439cc0c9ed978e2aceb39663
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-25 11:39:22 -0400

    implement smart link component, replace link imports

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link } from "../Link";
 import Stack from "@mui/joy/Stack";
 import Divider from "@mui/material/Divider";
 
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,28 +1,43 @@
-import React, { Fragment } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import { Link as GatsbyLink } from "gatsby";
 import { Link as MUILink } from "@mui/joy";
 
-// export const ExternalLinkIcon = ({}) => {
-//     return (
-//            <></>
-//     )
-// }
-
-export const ExternalLink = ({ to, children }) => {
+export const ExternalLink = ({ to, children, ...props }) => {
   return (
     <>
       <MUILink
         href={to}
         target="_blank"
-        // sx={{
-        //     ':hover': {
-
-        //     }
-        // }}
+        rel="noopener noreferrer"
+        underline="hover"
       >
         {children}
       </MUILink>
     </>
   );
 };
+
+export const InternalLink = ({ to, children, ref, ...props }) => {
+  return (
+    <MUILink component={GatsbyLink} to={to} {...props} ref={ref}>
+      {children}
+    </MUILink>
+  );
+};
+
+export const Link = React.forwardRef(({ to, children, ...props }, ref) => {
+  const externalUrlPattern = new RegExp(/^https?:\/\//);
+  const match = externalUrlPattern.exec(to);
+  if (match) {
+    return (
+      <ExternalLink to={to} {...props} ref={ref}>
+        {children}
+      </ExternalLink>
+    );
+  }
+  return (
+    <InternalLink to={to} {...props} ref={ref}>
+      {children}
+    </InternalLink>
+  );
+});
