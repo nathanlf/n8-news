9b2b154e409d3a1dd022ef60313730b0e50c0986
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-26 15:53:11 -0400

    add newsletter component skeleton, issue query wip

---
+++ src/components/Newsletter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useIssue } from "../hooks";
+
+// @param
+// edition: String, edition value ("4.1", "4.2", etc.)
+export const NewsletterIssue = ({ edition }) => {
+  return <></>;
+};
--- src/hooks/useIssue.js
+++ src/hooks/useIssue.js
@@ -1,9 +1,27 @@
 import { graphql, useStaticQuery } from "gatsby";
 
-// export const useIssue = () => {
-//   return useStaticQuery(graphql`
-//     query AllNewsletterIssues {
+/**
+ * This uses a static query to pull in all newsletter issue content.
+ *
+ * @param     {string}    edition      The edition identifier, `volume.issue`
+ * @return    ***
+ * */
 
-//     }
-//   `);
-// };
+export const useIssue = () => {
+  const allIssues = useStaticQuery(graphql`
+      query AllNewsletterIssues {
+        1: allMarkdownRemark(filter: {frontmatter: {volume: {eq: 1}}}) {
+            nodes {
+            
+            }
+        }
+        2: allMarkdownRemark(filter: {frontmatter: {volume: {eq: 2}}}) {
+            nodes {
+            
+            }
+        }
+      }
+    `);
+
+  return allIssues;
+};
