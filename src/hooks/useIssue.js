93753a6bcdee90c155cf121559cff2dcab6de956
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-27 10:55:05 -0400

    implement basic solution to use issue hook, plan approach to data structure

--- src/hooks/useIssue.js
+++ src/hooks/useIssue.js
@@ -3,25 +3,62 @@ import { graphql, useStaticQuery } from "gatsby";
 /**
  * This uses a static query to pull in all newsletter issue content.
  *
- * @param     {string}    edition      The edition identifier, `volume.issue`
- * @return    ***
+ * @param     {number}    volume      The edition volume identifier, corresponds to the year {2020 + volume}
+ * @param     {number}    issue       The edition issue identifier, corresponds to the month
+ * @return    {object}    The requested newsletter issue content, represented by edition {volume.issue}
  * */
 
-export const useIssue = () => {
+export const useIssue = ({ volume, issue }) => {
   const allIssues = useStaticQuery(graphql`
-      query AllNewsletterIssues {
-        1: allMarkdownRemark(filter: {frontmatter: {volume: {eq: 1}}}) {
-            nodes {
-            
-            }
-        }
-        2: allMarkdownRemark(filter: {frontmatter: {volume: {eq: 2}}}) {
-            nodes {
-            
-            }
+    query AllNewsletterIssues {
+      allMarkdownRemark {
+        nodes {
+          frontmatter {
+            volume
+            issue
+          }
+          id
         }
       }
-    `);
+    }
+  `);
 
-  return allIssues;
+  // todo:
+  //    - request more than just frontmatter and id, since we need the content we would use when rendering an issue
+  //        - i.e. rawMarkdownBody, ..., reference things in issue.js
+  //    - create custom data structure / collection to represent what we need in an organized way
+  /*
+        {
+            1: {    // volume 1
+                1: {},  // issue 1
+                2: {},
+                3: {},
+                ...
+            },
+            2: {    // volume 2
+                1: {},  // issue 1
+                2: {},
+                3: {},
+                ...
+            },
+            3: {    // volume 3
+                1: {},
+                2: {},
+                3: {},
+                ...
+            },
+            ...
+        }
+  */
+
+  const issueToReturn = allIssues.find(
+    (_issue) =>
+      volume === _issue.frontmatter.volume && issue === _issue.frontmatter.issue
+  );
+
+  if (!issueToReturn) {
+    throw new Error("Error returning issue");
+  }
+
+  return issueToReturn;
 };
