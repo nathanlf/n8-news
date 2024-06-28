3b22f5ebb5c8231ffa9adc50f3ccf42b12e28799
Matt Watson <mwatson@renci.org>
2024-06-28 11:37:32 -0400

    add lines bg component to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,20 @@ import "/src/styles/global.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Box } from "@mui/joy";
+import { styled } from "@mui/joy/styles";
 import { Container } from "./Container";
+import renciLinesSvg from '../../images/renci-lines.svg'
+
+const FancyBackground = styled('div')(() => ({
+  background: `linear-gradient(#ffffffee 75%, #ffffff66 100%), url(${ renciLinesSvg })`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '0 0',
+  backgroundSize: 'contain',
+  flexGrow: 1,
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
 
 export const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -20,17 +33,14 @@ export const Layout = ({ children }) => {
   `);
 
   return (
-    <>
-      <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head>
+    <FancyBackground>
       <Container>
         <Header title={data.site.siteMetadata.title} />
         <Navbar />
         {children}
       </Container>
       <Footer />
-    </>
+    </FancyBackground>
   );
 };
 
