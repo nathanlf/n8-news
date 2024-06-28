9989aaa16849e1b1a5d8b6fdaa4ca32b40a538b4
Matt Watson <mwatson@renci.org>
2024-06-28 11:36:33 -0400

    ensure main element is 800px wide

--- src/components/Layout/Container.js
+++ src/components/Layout/Container.js
@@ -8,7 +8,7 @@ export const Container = ({ children }) => {
       sx={{
         flexGrow: 1,
         margin: "0 auto",
-        maxWidth: "800px",
+        width: "800px",
       }}
     >
       {children}
