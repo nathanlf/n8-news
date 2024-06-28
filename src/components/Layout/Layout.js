9c48f075ffa17ab3ff08bcdc39bdbabac64c3664
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-28 11:48:30 -0400

    remove unused import

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,21 +5,19 @@ import PropTypes from "prop-types";
 import "/src/styles/global.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { Box } from "@mui/joy";
 import { styled } from "@mui/joy/styles";
 import { Container } from "./Container";
-import renciLinesSvg from '../../images/renci-lines.svg'
+import renciLinesSvg from "../../images/renci-lines.svg";
 
-const FancyBackground = styled('div')(() => ({
-  background: `linear-gradient(#ffffffee 75%, #ffffff66 100%), url(${ renciLinesSvg })`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: '0 0',
-  backgroundSize: 'contain',
+const FancyBackground = styled("div")(() => ({
+  background: `linear-gradient(#ffffffee 75%, #ffffff66 100%), url(${renciLinesSvg})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "0 0",
+  backgroundSize: "contain",
   flexGrow: 1,
-  display: 'flex',
-  flexDirection: 'column',
+  display: "flex",
+  flexDirection: "column",
 }));
-
 
 export const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
