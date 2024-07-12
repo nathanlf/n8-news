86230fa5ac468f6c5d3eeaeb70d13fa911c115b4
nathanlf <nathanlf@renci.org>
2024-07-12 13:34:22 -0400

    add color scheme, fix for primary color bug

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -20,4 +20,24 @@ export const theme = extendTheme({
       fontWeight: 700,
     },
   },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          50: "#EBFCFF",
+          100: "#DFFAFF",
+          200: "#5ce7ff",
+          300: "#0adaff",
+          400: "#02bdde",
+          500: "#02aac6",
+          600: "#0891b2",
+          700: "#0e7490",
+          800: "#155e75",
+          900: "#164e63",
+          plainColor: "var(--joy-palette-primary-500, #02aac6)",
+          plainHoverBg: "var(--joy-palette-primary-50, #DFFAFF)",
+        },
+      },
+    },
+  },
 });
