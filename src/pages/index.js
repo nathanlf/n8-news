9494dc0a52a5ba547e7671648545c6331add7a0b
nathanlf <nathanlf@renci.org>
2024-07-12 15:18:56 -0400

    query for max volume and issue number, wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import * as React from "react";
 import { Issue } from "../components/Issue/Issue";
+import { graphql } from "gatsby";
 
-const IndexPage = () => {
-  // as of now, the `vol` and `iss` props for this landing page are set manually
+const IndexPage = ({ data }) => {
+  const { maxVolume, maxIssue } = data.allMarkdownRemark;
 
   return (
     <>
-      <Issue vol={4} iss={5} />
+      <Issue vol={maxVolume} iss={maxIssue} />
     </>
   );
 };
+
+// todo: query for max issue of the max volume, not max issue of all issues
+
+export const query = graphql`
+  query VolumeNums {
+    allMarkdownRemark {
+      maxVolume: max(field: { frontmatter: { volume: SELECT } })
+      maxIssue: max(field: { frontmatter: { issue: SELECT } })
+    }
+  }
+`;
 
 export default IndexPage;
