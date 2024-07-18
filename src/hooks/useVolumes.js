46d78e055f70a79f260eb2ea9732df4eb1145a35
nathanlf <nathanlf@renci.org>
2024-07-18 10:49:22 -0400

    implement use volumes hook

---
+++ src/hooks/useVolumes.js
@@ -0,0 +1,20 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+export const useVolumes = () => {
+  // query returns volume numbers as strings
+  // for more on distinct queries, https://danielabaron.me/blog/gatsby5-distinct-query/
+  const data = useStaticQuery(graphql`
+    query VolumeNums {
+      allMarkdownRemark {
+        volumes: distinct(field: { frontmatter: { volume: SELECT } })
+      }
+    }
+  `);
+
+  // convert to integers, then convert to descending order (newest to oldest)
+  const volumes = data.allMarkdownRemark.volumes
+    .map((str) => parseInt(str))
+    .reverse();
+
+  return volumes;
+};
