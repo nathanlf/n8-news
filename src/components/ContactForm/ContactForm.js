b3d2d191dc65e7f3d1a888f0b475b01110f89965
nathanlf <nathanlf@renci.org>
2024-07-25 10:14:17 -0400

    link working appscripts code

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,92 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/joy";
-import { Close as SendIcon } from '@mui/icons-material';
-import { Controller, useFormContext, SubmitHandler, useForm } from 'react-hook-form';
-import * as styles from './ContactForm.module.css';
-import { Input } from './Input';
-import { TextArea } from './TextArea';
+import { Send as SendIcon } from "@mui/icons-material";
+import {
+  Controller,
+  useFormContext,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+import * as styles from "./ContactForm.module.css";
+import { Input } from "./Input";
+import { TextArea } from "./TextArea";
 
-const GOOGLE_APPS_SCRIPT_LINK = "URL";
+const GOOGLE_APPS_SCRIPT_LINK =
+  "https://script.google.com/macros/s/AKfycbxeB6s-NsD6jcs98qAamRziXyMg_Gt7g6uK8cdMqpmlPQZBYXVxWFJnRUkgH25LD_Ex/exec";
 
 export const ContactForm = () => {
   const {
     register,
     handleSubmit,
     reset,
-    formState: { isSubmitting }
+    formState: { isSubmitting },
   } = useForm();
-  const [response, setResponse] = useState("")
+  const [response, setResponse] = useState("");
 
   useEffect(() => {
-    const id = setTimeout(() => setResponse(""), 10_000)
+    const id = setTimeout(() => setResponse(""), 10_000);
     return () => clearTimeout(id);
-  }, [response])
+  }, [response]);
 
   const onSubmit = async (data) => {
     const form = new FormData();
     Object.entries(data).forEach(([label, response]) => {
       form.append(label, response);
-    })
+    });
 
     const res = await fetch(GOOGLE_APPS_SCRIPT_LINK, {
       body: form,
-      method: 'post'
-    })
+      method: "post",
+    });
 
     if (res.status !== 200) {
-      setResponse(res.statusText)
+      setResponse(res.statusText);
     } else {
-      setResponse("Your message has been submitted!")
+      setResponse("Your message has been submitted!");
     }
 
     reset();
-  }
+  };
 
   return (
     <>
-      {
-        response !== "" && <div className={styles.responseMessage}>
-          {response}
-        </div>
-      }
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit(onSubmit)}
-      >
+      {response !== "" && (
+        <div className={styles.responseMessage}>{response}</div>
+      )}
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.nameEmailGroup}>
-          <Input
-            label='Name'
-            required
-            type='text'
-            register={register}
-          />
+          <Input label="Name" required type="text" register={register} />
 
-          <Input
-            label='Email'
-            required
-            type='email'
-            register={register}
-          />
+          <Input label="Email" required type="email" register={register} />
         </div>
 
-        <Input
-          label='Options'
-          required
-          type='select'
-          register={register}
-        />
+        <Input label="Options" required type="select" register={register} />
 
-        <TextArea
-          label='Message'
-          required
-          rows={10}
-          register={register}
-        />
+        <TextArea label="Message" required rows={10} register={register} />
 
         <div className={styles.submitButtonWrapper}>
-          <p>This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy" target="_blank">Privacy Policy</a> and <a href="https://policies.google.com/terms" target="_blank">Terms of Service</a> apply.</p>
           <Button
-            type='submit'
+            type="submit"
             startDecorator={<SendIcon />}
             disabled={isSubmitting}
           >
@@ -95,5 +75,5 @@ export const ContactForm = () => {
         </div>
       </form>
     </>
-  )
-}
+  );
+};
