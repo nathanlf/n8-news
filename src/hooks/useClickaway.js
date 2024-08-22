6205d30e64532fc38d71cfdf7fa1e06163b43d50
nathanlf <nathanlf@renci.org>
2024-08-22 09:57:33 -0400

    document clickaway hook

--- src/hooks/useClickaway.js
+++ src/hooks/useClickaway.js
@@ -1,5 +1,20 @@
 import { useEffect } from "react";
 
+/**
+ * Triggers a callback function when a click is detected outside of the
+ * referenced element.
+ *
+ * This hook listens for `mousedown` events on the document and checks if the
+ * event's target is outside of the element referenced by `ref`. If it is, the
+ * provided `callback` function is executed.
+ *
+ * @param {Object}    ref         A React ref object attached to the element
+ *                                you want to detect clicks outside of.
+ * @param {Function}  callback    A function to be called when a click outside
+ *                                the referenced element is detected.
+ * @return {void}
+ */
+
 export const useClickAway = (ref, callback) => {
   useEffect(() => {
     const listener = (event) => {
@@ -8,9 +23,9 @@ export const useClickAway = (ref, callback) => {
       }
       callback(event);
     };
-    document.addEventListener('mousedown', listener);
+    document.addEventListener("mousedown", listener);
     return () => {
-      document.removeEventListener('mousedown', listener);
+      document.removeEventListener("mousedown", listener);
     };
   }, [ref, callback]);
 };
