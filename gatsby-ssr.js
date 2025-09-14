952c80b64ced5a1459b24fd2f5c747c69fca8e68
Nathan Flinchum <nathan@flinchum.net>
2025-09-13 21:42:48 -0400

    add dark mode preference script to ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,32 @@ import { Layout } from "./src/components/Layout/Layout";
 import { theme } from "./src/styles/theme";
 import { CssVarsProvider } from "@mui/joy";
 
+export const onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <script
+      key="set-initial-color-scheme"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            try {
+              var storedMode = localStorage.getItem('mui-mode');
+              if (storedMode) {
+                document.documentElement.setAttribute('data-mui-color-scheme', storedMode);
+              } else {
+                var prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                document.documentElement.setAttribute(
+                  'data-mui-color-scheme',
+                  prefersDark ? 'dark' : 'light'
+                );
+              }
+            } catch (e) {}
+          })();
+        `,
+      }}
+    />,
+  ]);
+};
+
 export const wrapPageElement = ({ element, props }) => {
   return (
     <CssVarsProvider theme={theme}>
